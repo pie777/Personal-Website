2a2207bd7c4ef4813a95900cd4e7a6af408e8ef0
pie777 <54085841+pie777@users.noreply.github.com>
2020-01-22 23:44:55 -0500

    Update introduction.jsx

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc" style={{textAlign:"right"}}>
                           <h1 style={{color:'white'}}>Hi! <br />I'm Anish</h1>
-                          <p><a style={{color:'white'}} className="btn btn-primary btn-learn" href="https://drive.google.com/open?id=1GnFVTkkCjt776hgtTHodgKNZI5dxgCbp" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <p><a style={{color:'white'}} className="btn btn-primary btn-learn" href="https://drive.google.com/open?id=165Iiq3bNnA6OJDBQcVtxPnU39aEbfT6Z" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
